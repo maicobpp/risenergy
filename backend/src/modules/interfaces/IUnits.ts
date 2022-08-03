b387d7c8784165cb726de6be6d837ff56c272bc9
Maico Beppler <maicobeppler@gmail.com>
2022-08-02 23:25:50 -0300

    units

---
+++ backend/src/modules/interfaces/IUnits.ts
@@ -0,0 +1,16 @@
+export interface IUnit {
+  id?: string;
+  name: string;
+  number: string;
+  orientation: string;
+  inclination: number;
+  fixed_cost?: number;
+  last_bill?: number;
+  minimun_charge: number;
+  customer_id: string;
+  data_id: string;
+}
+
+export interface IUnitID {
+  id: string;
+}
