4d84d48d82a78adbf2ceb7c9a22593ba90917f45
Maico Beppler <maicobeppler@gmail.com>
2022-10-30 00:00:01 -0300

    middleware checks

--- backend/src/middlewares/ensureAuthentication.ts
+++ backend/src/middlewares/ensureAuthentication.ts
@@ -5,7 +5,11 @@ interface IPayload {
   sub: string;
 }
 
-export async function ensureAuthentication(request: Request, response: Response, next: NextFunction) {
+export async function ensureAuthentication(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
