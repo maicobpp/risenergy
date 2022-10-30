0a97f91f83026eada8a023fad9a915681c244f3b
Maico Beppler <maicobeppler@gmail.com>
2022-10-30 00:00:02 -0300

    organization projects remove

--- backend/src/modules/organizations/interfaces/IProjectOrg.ts
+++
@@ -1,3 +0,0 @@
-export interface IProjectOrg {
-  org_id: string;
-}
--- backend/src/modules/organizations/routes.ts
+++ backend/src/modules/organizations/routes.ts
@@ -4,7 +4,6 @@ import { ensureAuthentication } from '../../middlewares/ensureAuthentication';
 import { AssociateOrganizationController } from './controllers/AssociateOrganizationControllerts';
 import { DeleteOrganizationController } from './controllers/DeleteOrganizationController';
 import { DissociateOrganizationController } from './controllers/DissociateOrganizationControllerts';
-import { FindOrganizationProjectsController } from './controllers/FindOrganizationProjectsController';
 import { StoreOrganizationController } from './controllers/StoreOrganizationController';
 
 const organizationsRoutes = Router();
@@ -13,13 +12,10 @@ const storeOrganizationController = new StoreOrganizationController();
 const deleteOrganizationController = new DeleteOrganizationController();
 const associateOrganizationController = new AssociateOrganizationController();
 const dissociateOrganizationController = new DissociateOrganizationController();
-const findOrganizationProjectsController = new FindOrganizationProjectsController();
 
 organizationsRoutes.post('/', ensureAuthentication, storeOrganizationController.handle);
 organizationsRoutes.post('/associate', ensureAuthentication, associateOrganizationController.handle);
 organizationsRoutes.post('/dissociate', ensureAuthentication, dissociateOrganizationController.handle);
-
-organizationsRoutes.get('/projects', ensureAuthentication, findOrganizationProjectsController.handle);
 
 organizationsRoutes.delete('/delete', ensureAuthentication, deleteOrganizationController.handle);
 
