cb332a13b43be06a6327a2f330e162684ca4de66
Maico Beppler <maicobeppler@gmail.com>
2022-11-01 00:00:01 -0300

    store unit

--- backend/src/modules/units/controllers/StoreUnitController.ts
+++ backend/src/modules/units/controllers/StoreUnitController.ts
@@ -1,20 +1,26 @@
 import { Request, Response } from 'express';
 
+import { StoreMonthDataService } from '../../monthData/services/StoreMonthDataService';
 import { StoreUnitService } from '../services/StoreUnitService';
 
 export class StoreUnitController {
   async handle(request: Request, response: Response) {
     const {
-      id, name, number, orientation, inclination, fixed_cost, last_bill, minimun_charge, customer_id, data_id,
+      id, name, number, fixed_cost, last_bill, minimun_charge, customer_id, data,
     } = request.body;
+
+    const storeMonthData = new StoreMonthDataService();
+    const resultMonthData = await storeMonthData.execute({
+      ...data,
+    });
+
+    const data_id = resultMonthData.id;
 
     const storeUnit = new StoreUnitService();
     const result = await storeUnit.execute({
       id,
       name,
       number,
-      orientation,
-      inclination,
       fixed_cost,
       last_bill,
       minimun_charge,
--- backend/src/modules/units/services/StoreUnitService.ts
+++ backend/src/modules/units/services/StoreUnitService.ts
@@ -3,7 +3,7 @@ import { IUnit } from '../interfaces/IUnit';
 
 export class StoreUnitService {
   async execute({
-    id, name, number, orientation, inclination, fixed_cost, last_bill, minimun_charge, customer_id, data_id,
+    id, name, number, fixed_cost, last_bill, minimun_charge, customer_id, data_id,
   }: IUnit) {
     const unit = await prisma.units.upsert({
       where: {
@@ -12,8 +12,6 @@ export class StoreUnitService {
       update: {
         name,
         number,
-        orientation,
-        inclination,
         fixed_cost,
         last_bill,
         minimun_charge,
@@ -23,8 +21,6 @@ export class StoreUnitService {
       create: {
         name,
         number,
-        orientation,
-        inclination,
         fixed_cost,
         last_bill,
         minimun_charge,
