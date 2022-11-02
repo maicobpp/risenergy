e305e47255551164b118aa65d8fcc1c766203b89
Maico Beppler <maicobeppler@gmail.com>
2022-11-02 00:00:01 -0300

    custom input ajustments

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,6 +1,7 @@
 import {
   FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps,
 } from '@chakra-ui/react';
+import InputMask from 'react-input-mask';
 
 interface InputProps extends ChakraInputProps {
   name: string;
@@ -10,9 +11,11 @@ interface InputProps extends ChakraInputProps {
 export function Input({ name, label, ...rest }: InputProps) {
   return (
     <FormControl>
-      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      {!!label && <FormLabel fontSize="sm" htmlFor={name}>{label}</FormLabel>}
 
       <ChakraInput
+        as={InputMask}
+        masrk="9999999,99"
         name={name}
         id={name}
         focusBorderColor="green.400"
@@ -21,7 +24,7 @@ export function Input({ name, label, ...rest }: InputProps) {
         _hover={{
           bgColor: 'gray.900',
         }}
-        size="md"
+        size="sm"
         {...rest}
       />
     </FormControl>
