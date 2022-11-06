2e2d82064cf019160f2b26af31bc12c89f76da9a
Maico Beppler <maicobeppler@gmail.com>
2022-11-06 00:00:02 -0300

    remove old sleep

--- frontend/src/lib/sleep.ts
+++
@@ -1,8 +0,0 @@
-export default function sleep(ms: number) {
-  return new Promise(
-    (resolve) => {
-      // eslint-disable-next-line no-promise-executor-return
-      return setTimeout(resolve, ms);
-    },
-  );
-}
