9e235a82318071b8b629192260e3eeea1293d563
Maico Beppler <maicobeppler@gmail.com>
2022-11-16 00:00:01 -0300

    dashboard ajustments

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,70 +1,35 @@
 import { Box, SimpleGrid, Text } from '@chakra-ui/react';
 import Chart from 'react-apexcharts';
 import { theme } from '../styles/theme';
-
-const options = {
-  chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[400],
-  },
-  grid: {
-    show: false,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
-  xaxis: {
-    axisBorder: {
-      color: theme.colors.gray[600],
-    },
-    axisTicks: {
-      color: theme.colors.gray[600],
-    },
-    categories: [
-      'Mar',
-      'Abr',
-      'Mai',
-      'Jun',
-      'Jul',
-      'Ago',
-      'Set',
-    ],
-  },
-  fill: {
-    type: 'gradient',
-    gradient: {
-      type: 'vertical',
-      shade: 'dark',
-    },
-  },
-  colors: [
-    '#0FDB54',
-  ],
-};
-
-const series = [
-  { name: 'orcamentos', data: [8, 10, 11, 3, 5, 8, 2] },
-];
+import {
+  aprovados, carga, ChartOptions, orcamentos, total,
+} from '../utils/charts';
 
 export function Dashboard() {
   return (
-    <SimpleGrid flex="1" gap="4" minChildWidth="400px">
+    <SimpleGrid
+      flex="1"
+      gap="4"
+      minChildWidth="400px"
+    >
       <Box
         p="8"
         bg="gray.800"
         borderRadius={8}
         pb="4"
       >
-        <Text fontSize="lg" mb="4">Total Orçamentos</Text>
-        <Chart options={options} series={series} type="bar" height={160} />
+        <Text
+          fontSize="lg"
+          mb="4"
+        >
+          Total Orçamentos
+        </Text>
+        <Chart
+          options={ChartOptions(theme.colors.cyan[400])}
+          series={orcamentos}
+          type="bar"
+          height={160}
+        />
       </Box>
 
       <Box
@@ -72,7 +37,8 @@ export function Dashboard() {
         bg="gray.800"
         borderRadius={8}
       >
-        <Text fontSize="lg" mb="4">Total de Projetos</Text>
+        <Text fontSize="lg" mb="4">Total de Projetos Aprovados</Text>
+        <Chart options={ChartOptions(theme.colors.green[400])} series={aprovados} type="bar" height={160} />
       </Box>
 
       <Box
@@ -80,7 +46,8 @@ export function Dashboard() {
         bg="gray.800"
         borderRadius={8}
       >
-        <Text fontSize="lg" mb="4">Taxa de Aprovação</Text>
+        <Text fontSize="lg" mb="4">Carga Instalada (kWh)</Text>
+        <Chart options={ChartOptions(theme.colors.orange[400])} series={carga} type="bar" height={160} />
       </Box>
 
       <Box
@@ -88,7 +55,8 @@ export function Dashboard() {
         bg="gray.800"
         borderRadius={8}
       >
-        <Text fontSize="lg" mb="4">KWh Instalados</Text>
+        <Text fontSize="lg" mb="4">Total em KWh Instalados</Text>
+        <Chart options={ChartOptions(theme.colors.pink[400])} series={total} type="area" height={160} />
       </Box>
     </SimpleGrid>
   );
