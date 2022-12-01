89c38eab806e960c9fd36e379b99a9678d18eb46
Maico Beppler <maicobeppler@gmail.com>
2022-12-01 09:26:14 -0300

    input mask

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,21 +1,44 @@
 import {
   FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps,
 } from '@chakra-ui/react';
-import InputMask from 'react-input-mask';
+import ReactInputMask from 'react-input-mask';
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  mask?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+export function Input({
+  name, label, mask, ...rest
+}: InputProps) {
+  if (mask) {
+    return (
+      <FormControl>
+        {!!label && <FormLabel fontSize="sm" htmlFor={name}>{label}</FormLabel>}
+
+        <ChakraInput
+          as={ReactInputMask}
+          mask={mask}
+          name={name}
+          id={name}
+          focusBorderColor="green.400"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: 'gray.900',
+          }}
+          size="sm"
+          {...rest}
+        />
+      </FormControl>
+    );
+  }
   return (
     <FormControl>
       {!!label && <FormLabel fontSize="sm" htmlFor={name}>{label}</FormLabel>}
 
       <ChakraInput
-        as={InputMask}
-        masrk="9999999,99"
         name={name}
         id={name}
         focusBorderColor="green.400"
